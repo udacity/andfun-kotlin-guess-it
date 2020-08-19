438b83ffe9b5bb34656aa73c1dd25de3b3e4e298
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-19 19:26:03 +0530

    Update Gradle scripts

--- Upload.sh
+++ Upload.sh
@@ -4,9 +4,10 @@ if [ $# != 1 ]; then
     exit;
 fi
 
-branches=`git for-each-ref --format='%(refname:short)' refs/heads/\*`
+branches=`git for-each-ref --shell --format='%(refname)' refs/heads/\*`
 curr_branch=`git rev-parse --abbrev-ref HEAD`
 echo "curr_branch:"$curr_branch
+# eval "$(git for-each-ref --shell --format='branches+=(%(refname))' refs/heads/)"
 
 filename=$1
 file_in_repo=`git ls-files ${filename}`
@@ -19,11 +20,11 @@ fi
 for branch in "${branches[@]}"; do
   if [[ "${branch}" != "master" ]]; then
     git checkout "${branch}"
-    git checkout master build.gradle
+    git checkout master -- build.gradle
 	#git checkout master app/build.gradle
 	#git checkout master gradle/wrapper/gradle-wrapper.properties
-	git add . 
-	git commit -m "Update Gradle scripts"
+	git add build.gradle
+	git commit -am "Update Gradle scripts"
 	git push
   fi
 done
