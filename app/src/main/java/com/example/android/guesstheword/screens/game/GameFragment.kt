40b9d951d0b722a0179bcd1316e67783bcea19c4
SudKul <sudhanshu.kulshrestha@gmail.com>
2020-08-24 14:23:07 +0530

    Changed the deprecated ViewModelProviders (plural) using a script.

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -52,7 +52,7 @@ class GameFragment : Fragment() {
         )
 
         // Get the viewmodel
-        viewModel = ViewModelProviders.of(this).get(GameViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(GameViewModel::class.java)
 
         // Set the viewmodel for databinding - this allows the bound layout access to all of the
         // data in the VieWModel
